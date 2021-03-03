1c05a623cb10cb2f55f65bba383f55f74b915a23
ehsann95 <psychoahsan@gmail.com>
2021-03-03 16:44:37 +0530

    random orderID and signature in params

--- index.js
+++ index.js
@@ -3509,10 +3509,10 @@
   window.addEventListener("message", removeIframe, false);
 
   function removeIframe(e) {
-    const { callback, transactionId, payCapture, orderId } = e.data;
-    console.log(e.data);
+    const { callback, transactionId, orderId, key } = e.data;
+    console.log("REMOVE IFRAME ==== ", e.data);
     document.getElementById("iframe").innerHTML = "";
-    window.location.href = `${callback}?transaction_id=${transactionId}&order_id=${orderId}&payment_capture=${payCapture}`;
+    window.location.href = `${callback}?transaction_id=${transactionId}&order_id=${orderId}&signature=${key}`;
   }
 
   npg.prototype = {
